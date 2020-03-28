d1e9656395b961298239c442694f228606f86b47
david knigge <davidknigge@live.nl>
2020-03-28 21:45:55 +0100

    Update prismizer.py

--- prismizer.py
+++ prismizer.py
@@ -1,11 +1,11 @@
 
-from flask import Flask
+from flask import Flask, request
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path="")
 
 @app.route('/')
 def index():
-    return app.send_static_file('./index.html')
+    return app.send_static_file('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=2020)
